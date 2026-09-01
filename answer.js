4ace81800b9fbd2826d982f82f262836ae6686d9
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2026-09-01 23:14:11 +0600

    solve question 2

--- answer.js
+++ answer.js
@@ -8,3 +8,29 @@ return `${typeof value} | ${Boolean(value) ? "truthy" : "falsy"}`;
 // console.log(Boolean("shr"));
 // console.log(Boolean(""));
 
+
+
+/* Question 2. Write a function getDayType that takes a day name and returns:
+Friday, Saturday =	"Weekend"
+Sunday, Monday, Tuesday, Wednesday, Thursday =	"Working Day"
+anything else =	"Invalid Day"
+*/
+function getDayType(dayParam){
+    const day = dayParam.toLowerCase();
+    switch(day){
+        case "friday":
+        case "saturday":
+            return `Weekend`;
+        case "sunday":
+        case "monday":
+        case "tuesday":
+        case "wednesday":
+        case "thursday":
+            return `Working Day`
+        default:
+            return `Invalid Day`;
+    }
+}
+
+// for check purpose
+// console.log(getDayType("sundayd"));
